873deff65ec6b2c7ca8770cbcf2dab9d3c4a64ad
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2023-04-22 21:57:34 +0100

    Update salaryCalculator.js

--- salaryCalculator.js
+++ salaryCalculator.js
@@ -2,15 +2,23 @@ var name = process.argv[2]
 var age = parseInt(process.argv[3])
 var basicSalary = parseFloat(process.argv[4])
 
-const hra = (50*basicSalary)/100
+if (age <= 0) {
+    console.log(`Invalid age, enter a valid age`);
+} else {
+    if (basicSalary <= 0) {
+        console.log(`Invalid salary, enter a valid salry`); 
+    } else {
+        const hra = (50*basicSalary)/100
 
-const specialAllowance = (30*basicSalary)/100
+        const specialAllowance = (30*basicSalary)/100
 
-const pf = (12*basicSalary)/100
+        const pf = (12*basicSalary)/100
 
-let grossSalary = (basicSalary+ hra + specialAllowance - pf)
+        let grossSalary = (basicSalary+ hra + specialAllowance - pf)
 
 
-let annualGrossSalary = 12 * basicSalary
+        let annualGrossSalary = 12 * basicSalary
 
-console.log(`The annual gross salary of ${name} is ${annualGrossSalary}`)
+        console.log(`The annual gross salary of ${name} is ${annualGrossSalary}`)
+    }
+}
