185fa84ca7e8c36526d4140521eb4f68f8975dad
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2023-04-23 08:04:36 +0100

    update

---
+++ area-calculator/src/areacalculator.js
@@ -0,0 +1,23 @@
+const pi = 3.14;
+
+const calculateArea = (choice,side,length,breadth,radius) =>{   
+    let area = 0.0; 
+    // write logic here
+    // note that you check the values passed are not null before performing any operation on them
+    switch (choice) {
+        case 'square':
+            
+            break;
+        case 'circle':
+
+        break
+        case 'rectangle':
+
+        break
+    
+        default:
+            return -1;
+    }
+    return area
+}
+module.exports = {calculateArea}
