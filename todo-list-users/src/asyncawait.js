a131c969a761e10cd9bbe968e0006bc6b7162b26
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2023-04-26 07:37:00 +0100

    tests passed

--- todo-list-users/src/asyncawait.js
+++ todo-list-users/src/asyncawait.js
@@ -1,21 +1,45 @@
 const users = require('./users')
 // Define a function that returns a promise to get all users and return a promise
 const getAllUsers = ()=>{
-    
+    return new Promise((resolve, reject) => {
+
+        resolve(users);
+    });
  
 }
 //Define a function to create a new user and return a promise
-const createUser = (user) => {
-    
- }
+const createUser = async (user) => {
+    return new Promise((resolve, reject) =>{
+        if(user === undefined){
+            reject('No user to be added');
+        };
+
+        id = user.id;
+        users.push(user);
+        resolve(users[users.length-1]);
+ })}
 // Define a function to get a user by id and return a promise
 const getAUserByID = (id) =>{
-    
+
+    return new Promise((resolve, reject) => {
+        var u = [];
+        for (let index = 0; index < users.length; index++) {
+            if (users[index].id == id){
+                u.push(users[index]);
+                console.log(u);
+            }
+
+        if(u){
+            resolve(u);
+        }
+        reject("User does not exist")
+    };
+ });
 }
 // Define an async function that calls the createUser and getAllUsers function using await 
 // and returns all users
 const displayUsers = async (user) => {
-   const allUsers = null;
+   const allUsers = await getAllUsers();
    return allUsers;
 }
 //Define a async function to display a specific user by Id
@@ -24,7 +48,6 @@ const displayAUser = async(id) =>{
     const user = null;
     return user;
 }
-
 module.exports = {
     getAllUsers, getAUserByID, createUser, displayAUser, displayUsers
 }
