388d85ba504fd249820fe98f5924b244f7b4efc5
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2023-04-29 07:20:15 +0100

    upadte

---
+++ Node-error-handling/voterAgeValidation.js
@@ -0,0 +1,25 @@
+const { AgeError } = require("./ageException")
+
+const ageValidator= (age) => {
+    try{
+        if (age < 0){
+            throw new AgeError('Age cannot be negative');
+        }
+        if (age >= 0 && age < 18){
+            throw new AgeError('Age is invalid for voting')
+        }
+
+        console.log('Age is valid');
+    }
+    catch(err){
+        console.log(err.name);
+        console.log(err.message);
+        console.log(err.stack);
+    }
+    finally{
+        console.log('Age is validated');
+    }
+};
+
+
+ageValidator(13);
