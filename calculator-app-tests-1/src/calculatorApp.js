b6ed239c6d43133c435cfc0dc4272979c692b5a0
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2023-04-29 19:09:33 +0100

    tests passed

---
+++ calculator-app-tests-1/src/calculatorApp.js
@@ -0,0 +1,19 @@
+const addition = (num1, num2) => {
+  // Write the code here
+  return num1+num2
+};
+
+const subtraction = (num1, num2) => {
+    // Write the code here
+    return num1-num2
+};
+
+const multiplication = (num1, num2) => {
+  // Write the code here
+};
+
+const division = (num1, num2) => {
+  // Write the code here
+};
+
+module.exports = {addition, subtraction, multiplication, division };
